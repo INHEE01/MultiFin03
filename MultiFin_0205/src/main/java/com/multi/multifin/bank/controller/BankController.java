6a6f4977224808d6273a2ad2ba01da454d89b20b
Yebin Wang <qhqhdhkd@gmail.com>
2023-02-14 22:37:13 +0900

    왕예빈/은행메인, 은행 검색(bankSearch) 완료/2023-02-14 22:37

--- MultiFin_0205/src/main/java/com/multi/multifin/bank/controller/BankController.java
+++ MultiFin_0205/src/main/java/com/multi/multifin/bank/controller/BankController.java
@@ -298,8 +298,178 @@ public class BankController {
 	}
 
 	@RequestMapping("/bankMain")
-	public String bankMain() {
+	public String bankMain(Model model, Map<String, String> paramMap) {
+		log.info("신용카드 전체 요청");
+		int pageDebit = 1;
+		try {
+			String searchValue = paramMap.get("searchValue");
+			if(searchValue != null && searchValue.length() > 0) {
+				paramMap.put("companyNm", searchValue);
+			}else {
+			}
+			pageDebit = Integer.parseInt(paramMap.get("pageDebit"));
+		} catch (Exception e) {
+		}
+		
+		int debitCount = cardService.getDebitCount(paramMap);
+		PageInfo pageDebitInfo = new PageInfo(pageDebit, 5, debitCount, 24);
+		List<BankDebitCard> debitList = cardService.selectDebitList(pageDebitInfo, paramMap);
+		model.addAttribute("debitList", debitList);
+		model.addAttribute("paramMap", paramMap);
+		model.addAttribute("pageDebitInfo", pageDebitInfo);
+		
+		log.info("체크카드 전체 요청");
+		int pageCredit = 1;
+		try {
+			pageCredit = Integer.parseInt(paramMap.get("pageCredit"));
+		} catch (Exception e) {
+		}
+		int creditCount = cardService.getCreditCount(paramMap);
+		PageInfo pageCreditInfo = new PageInfo(pageCredit, 5, creditCount, 24);
+		List<BankCreditCard> creditList = cardService.selectCreditList(pageCreditInfo, paramMap);
+		model.addAttribute("creditList", creditList);
+		model.addAttribute("pageCreditInfo", pageCreditInfo);
+		
+		log.info("예적금상품 요청");
+		List<BankDeposit> depositList0 = bankbookService.selectDepositList();
+		List<BankSaving> savingList0 = bankbookService.selectSavingList();
+		List<BankCompany> logoList = companyService.selectLogoList(paramMap);
+		model.addAttribute("logoList", logoList);
+		
+		for(BankDeposit deposit : depositList0) {
+			String coName = deposit.getKorCoNm();
+			for(BankCompany logoInfo :  logoList) {
+				if(logoInfo.getKorCoNm().equals(coName)) {
+					deposit.setLogoUrl(logoInfo.getLogoUrl());
+					break;
+				}
+			}
+		}
+		for(BankSaving saving : savingList0) {
+			String coName = saving.getKorCoNm();
+			for(BankCompany logoInfo :  logoList) {
+				if(logoInfo.getKorCoNm().equals(coName)) {
+					saving.setLogoUrl(logoInfo.getLogoUrl());
+					break;
+				}
+			}
+		}
+		model.addAttribute("depositList0", depositList0);
+		model.addAttribute("savingList0", savingList0);
+		
+		log.info("대출 상품 요청 성공");
+		List<LoanCredit> loanCreditList0 = lcService.selectLoanCreditList();
+		model.addAttribute("loanCreditList0", loanCreditList0);
+		List<LoanMortgage> loanMortgageList0 = lmService.selectLoanMortgageList();
+		model.addAttribute("loanMortgageList0", loanMortgageList0);
+		List<LoanRentHouse> loanRentHoustList0 = lrhService.selectLoanRentHouseList();
+		model.addAttribute("loanRentHoustList0", loanRentHoustList0);
+		
 		return "bank/bankMain";
+	}
+	
+	@RequestMapping("/bankSearch")
+	public String bankSearch(Model model, @RequestParam("searchValue") String param, Map<String, String> paramMap) {
+		model.addAttribute("searchValue", param);
+		
+		log.info("예금상품 전체 요청");
+		int pageDeposit = 1;
+		try {
+			paramMap.put("korCoNm", param);
+			pageDeposit = Integer.parseInt(paramMap.get("pageDeposit"));
+		} catch (Exception e) {
+		}
+		
+		int depositCount = bankbookService.getDepositCountUnique(paramMap);
+		PageInfo pageDepositInfo = new PageInfo(pageDeposit, 5, depositCount, 10);
+		List<BankDeposit> depositList = bankbookService.selectDepositListUnique(pageDepositInfo, paramMap);
+		
+		model.addAttribute("depositList", depositList);
+		model.addAttribute("paramMap", paramMap);
+		model.addAttribute("pageDepositInfo", pageDepositInfo);
+		
+		log.info("적금상품 전체 요청");
+		int pageSaving = 1;
+		try {
+			pageSaving = Integer.parseInt(paramMap.get("pageSaving"));
+		} catch (Exception e) {
+		}
+		int savingCount = cardService.getCreditCount(paramMap);
+		PageInfo pageSavingInfo = new PageInfo(pageSaving, 5, savingCount, 10);
+		List<BankSaving> savingList = bankbookService.selectSavingListUnique(pageSavingInfo, paramMap);
+		
+		model.addAttribute("savingList", savingList);
+		model.addAttribute("pageSavingInfo", pageSavingInfo);
+		
+		log.info("개인신용대출 전체 요청");
+		int pageLc = 1;
+		try {
+			paramMap.put("korCoNm", param);
+			pageLc = Integer.parseInt(paramMap.get("pageLc"));
+		} catch (Exception e) {
+		}
+		
+		int lcCount = lcService.selectLoanCreditCountUnique(paramMap);
+		PageInfo pageLcInfo = new PageInfo(pageLc, 5, lcCount, 10);
+		List<LoanCredit> loanCreditList0 = lcService.selectLoanCreditListUnique(pageLcInfo, paramMap);
+		model.addAttribute("loanCreditList0", loanCreditList0);
+		model.addAttribute("paramMap", paramMap);
+		model.addAttribute("pageLcInfo", pageLcInfo);
+		
+		log.info("주택담보대출 전체 요청");
+		int pageLm = 1;
+		try {
+			pageLm = Integer.parseInt(paramMap.get("pagdLm"));
+		} catch (Exception e) {
+		}
+		int lmCount = lmService.selectLoanMortgageCountUnique(paramMap);
+		PageInfo pageLmInfo = new PageInfo(pageLm, 5, lmCount, 10);
+		List<LoanMortgage> loanMortgageList0 = lmService.selectLoanMortgageListUnique(pageLmInfo, paramMap);
+		model.addAttribute("loanMortgageList0", loanMortgageList0);
+		model.addAttribute("pageLmInfo", pageLmInfo);
+		
+		log.info("전세자금대출 전체 요청");
+		int pageLrh = 1;
+		try {
+			pageLrh = Integer.parseInt(paramMap.get("pageLrh"));
+		} catch (Exception e) {
+		}
+		int lrhCount = lrhService.selectLoanRentHouseCountUnique(paramMap);
+		PageInfo pageLrhInfo = new PageInfo(pageLrh, 5, lrhCount, 10);
+		List<LoanRentHouse> loanRentHoustList0 = lrhService.selectLoanRentHouseListUnique(pageLmInfo, paramMap);
+		model.addAttribute("loanRentHoustList0", loanRentHoustList0);
+		model.addAttribute("pageLrhInfo", pageLrhInfo);
+		
+		
+		
+		log.info("신용카드 전체 요청");
+		int pageDebit = 1;
+		try {
+				paramMap.put("companyNm", param);
+			pageDebit = Integer.parseInt(paramMap.get("pageDebit"));
+		} catch (Exception e) {
+		}
+		
+		int debitCount = cardService.getDebitCount(paramMap);
+		PageInfo pageDebitInfo = new PageInfo(pageDebit, 5, debitCount, 12);
+		List<BankDebitCard> debitList = cardService.selectDebitList(pageDebitInfo, paramMap);
+		model.addAttribute("debitList", debitList);
+		model.addAttribute("paramMap", paramMap);
+		model.addAttribute("pageDebitInfo", pageDebitInfo);
+		
+		log.info("체크카드 전체 요청");
+		int pageCredit = 1;
+		try {
+			pageCredit = Integer.parseInt(paramMap.get("pageCredit"));
+		} catch (Exception e) {
+		}
+		int creditCount = cardService.getCreditCount(paramMap);
+		PageInfo pageCreditInfo = new PageInfo(pageCredit, 5, creditCount, 12);
+		List<BankCreditCard> creditList = cardService.selectCreditList(pageCreditInfo, paramMap);
+		model.addAttribute("creditList", creditList);
+		model.addAttribute("pageCreditInfo", pageCreditInfo);
+		
+		return "bank/bankSearch";
 	}
 	
 }
