25fddc17d5d06eb99b5ccd40d9b7010a19a5d249
user <user@DESKTOP-2I49R3I>
2023-02-17 10:39:48 +0900

    2023-02-17 박원철 법률페이지 수정1

--- MultiFin_0205/src/main/java/com/multi/multifin/law/mapper/LawQnaMapper.java
+++
@@ -1,9 +0,0 @@
-package com.multi.multifin.law.mapper;
-
-public class LawQnaMapper {
-//	List<LawQna> lawMain();
-//	LawQna selectLawById(int id);
-//    void insertLaw(LawQna law);
-//    void updateLaw(LawQna law);
-//    void deleteLaw(int id);
-}
--- MultiFin_0205/src/main/java/com/multi/multifin/law/vo/LawQna.java
+++
@@ -1,9 +0,0 @@
-package com.multi.multifin.law.vo;
-
-public class LawQna {
-	private int id; // Law 테이블의 id 컬럼
-    private String title; // Law 테이블의 title 컬럼
-    private String content; // Law 테이블의 content 컬럼
-    
-    // getter, setter 메서드 생략
-}
