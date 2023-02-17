915db5c9b28d5e3a70181d2c8907509624c1dd4a
user <user@DESKTOP-2I49R3I>
2023-02-17 09:36:26 +0900

    20223-02-17 박원철 법률 페이지 일부수정

--- MultiFin_0205/src/main/java/com/multi/multifin/law/mapper/LawQnaMapper.java
+++ MultiFin_0205/src/main/java/com/multi/multifin/law/mapper/LawQnaMapper.java
@@ -1,5 +1,9 @@
 package com.multi.multifin.law.mapper;
 
 public class LawQnaMapper {
-
+//	List<LawQna> lawMain();
+//	LawQna selectLawById(int id);
+//    void insertLaw(LawQna law);
+//    void updateLaw(LawQna law);
+//    void deleteLaw(int id);
 }
--- MultiFin_0205/src/main/java/com/multi/multifin/law/service/LawQnaService.java
+++ MultiFin_0205/src/main/java/com/multi/multifin/law/service/LawQnaService.java
@@ -1,5 +1,34 @@
 package com.multi.multifin.law.service;
 
-public class LawQnaService {
+import org.springframework.stereotype.Service;
 
+import com.multi.multifin.law.mapper.LawQnaMapper;
+import com.multi.multifin.law.vo.LawQna;
+
+@Service
+public class LawQnaService {
+//	 
+//    public LawQnaService(LawQnaMapper lawQnaMapper) {
+//        this.LawQnaMapper = lawQnaMapper;
+//    }
+//    
+//    public List<LawQna> getAllLaws() {
+//        return lawMapper.selectAllLaws();
+//    }
+//    
+//    public LawQna getLawById(int id) {
+//        return LawQnaMapper.selectLawById(id);
+//    }
+//    
+//    public void addLaw(LawQna law) {
+//    	LawQnaMapper.insertLaw(law);
+//    }
+//    
+//    public void updateLaw(LawQna law) {
+//    	LawQnaMapper.updateLaw(law);
+//    }
+//    
+//    public void deleteLaw(int id) {
+//    	LawQnaMapper.deleteLaw(id);
+//    }
 }
