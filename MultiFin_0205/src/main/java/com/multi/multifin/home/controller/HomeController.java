3c86cdf159c5a62e13cd5664765a629936e79111
박지나 <@host.docker.internal>
2023-02-18 14:57:45 +0900

    박지나 / 청약페이지 체크박스 순서 변경/2023-02-18 14:57

--- MultiFin_0205/src/main/java/com/multi/multifin/home/controller/HomeController.java
+++ MultiFin_0205/src/main/java/com/multi/multifin/home/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequestMapping("/home")
-@org.springframework.stereotype.Controller
+@Controller
+
 public class HomeController {
 
 	@Autowired
